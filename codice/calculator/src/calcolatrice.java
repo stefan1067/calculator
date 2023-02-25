46cb91c4ba405db316da7d04eef4a73869daf70e
stefan1067 <92978849+stefan1067@users.noreply.github.com>
2023-02-25 15:21:59 +0100

    calcolatrice
    Calcolatrice con caricamento

---
+++ codice/calculator/src/calcolatrice.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class calcolatrice {
+public static void main(String[] args) throws IOException {
+	InputStreamReader input = new InputStreamReader(System.in);
+	BufferedReader tastiera = new BufferedReader(input);
+		int risultato=0;
+		System.out.println("Decidi operazione da svolgere");
+		String operazione;
+		operazione=tastiera.readLine();
+		System.out.println(operazione);
+		System.out.println("inserisci i 2 numeri con i quali effettuare le operazioni");
+		Scanner n;
+		n=new Scanner(System.in);
+		int num1=n.nextInt();
+		int num2=n.nextInt();
+		
+		//somma
+		if(operazione.equals("somma")){
+		
+		System.out.println(num1+num2);
+		
+		}
+		
+		//divione
+		if(operazione.equals("divisione")){
+			int c=num1/num2;
+			System.out.println(num1/num2);
+			
+			}
+		
+		//moltiplicazione
+		if(operazione.equals("moltiplicazione")){
+			int c=num1/num2;
+			System.out.println(num1*num2);
+			
+			}
+		
+		if(operazione.equals("sottrazione")){
+			int c=num1/num2;
+			System.out.println(num1-num2);
+			
+			}
+				
+	}
+}
